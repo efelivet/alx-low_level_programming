5c03a601953daf5160420902bd0edf61098c5a10
efelivet <enobakharerichie@gmail.com>
2023-03-20 12:25:21 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
-
-#define ABS(x) (x * ((x < 0) * (-1) + (x > 0)))
-
+#ifndef absolute_value
+#define absolute_value
+#define ABS(x) (((x) < 0) ? (x) * (-1) : (x))
 #endif
